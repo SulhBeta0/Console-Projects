1bb24954e43207961ea73bc30fe0f501c4d6abb7
Sami <amasamira14@gmail.com>
2025-10-29 19:46:45 +0100

    New tokens system + Updating functions

--- PJ12_Text_Calculator/Program.cs
+++ PJ12_Text_Calculator/Program.cs
@@ -1,17 +1,14 @@
 ﻿//Finished on ??? 0th, 2025 -
 
-using System.Runtime.Versioning;
 namespace PJ12_Text_Calculator;
 
 public class Program
 {
     static void Main()
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("HAY ERRORES :v\nDE MOMENTO NO HAY JERARQUÍA EN OPERACIONES ASI QUE LOS CALCULOS PODRÁN ESTAR MAL SEGUN QUE OPERACIONES SE HAGA\n");
-        Console.ResetColor();
+        Calculator calculator = new();
+        calculator.Update();
 
-        Calculator.Update();
         Environment.Exit(0);
     }
 }
--- PJ12_Text_Calculator/TextProcessor.cs
+++ PJ12_Text_Calculator/TextProcessor.cs
@@ -1,69 +1,29 @@
 ﻿namespace PJ12_Text_Calculator;
 
-#pragma warning disable CA1416
 internal static class TextProcessor
 {
+
     internal static string Text()
     {
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        Console.Write("Escriba --COMMANDS-- como ayuda => ");
+        Messages("<<help:\n", ConsoleColor.DarkGray);
+        Messages("C://Users//Calculator.exe> ", ConsoleColor.DarkGreen);
 
-        string text = Console.ReadLine()!.ToUpper()!;
-        Console.ResetColor();
+        string text = Console.ReadLine()?.ToUpper()!;
 
-        string secureText = String.IsNullOrWhiteSpace(text) ? "0" : text;
-        return secureText;
+        //string secureText = String.IsNullOrWhiteSpace(text) ? " " : text;
+        return text;
     }
 
-    internal static (List<double> numbers, List<string> operations) SplitText
-        (string myText, double? acumulator, List<double> listNumbers, List<string> listOperations)
-    {
-        int countOperations = 0;
-
-        //string[] textSortedAndSplited = SortingText(myText).Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        string[] textSplit = myText.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-        if (!acumulator.HasValue && !double.TryParse(textSplit[0], out _))
-        {
-            Console.Beep(450, 300);
-            return (new List<double>(), new List<string>());
-        }
-
-        for (int i = 0; i < textSplit.Length; i++)
-        {
-            if (double.TryParse(textSplit[i], out double validNum))
-            {
-                listNumbers.Add(validNum);
-                continue;
-            }
-
-            if (Calculator.MyActions.Contains<string>(textSplit[i]) &&
-                countOperations <= Calculator._maxConsecutivesOperations)
-            {
-                listOperations.Add(textSplit[i]);
-                countOperations++;
-            }
-        }
-
-        if (listOperations.Count < 1 || listOperations.Count > Calculator._maxConsecutivesOperations)
-        {
-            Console.Beep(450, 300);
-            return (new List<double>(), new List<string>());
-        }
-
-        return (numbers: listNumbers, operations: listOperations);
-    }
-
-    internal static void Commands()
+    internal static void Commands(string[] actions)
     {
         Console.Clear();
-        Console.ForegroundColor = ConsoleColor.DarkCyan;
-        Console.WriteLine("=> " + String.Join(" | ", Calculator.MyActions) + "\n");
-        Console.ResetColor();
+        Messages("=> /" + String.Join(" /", actions) + "\n", ConsoleColor.White);
     }
 
-    static string SortingText(string textToSort)
+    internal static void Messages(string myText, ConsoleColor color)
     {
-        return textToSort;
+        Console.ForegroundColor = color;
+        Console.Write(myText);
+        Console.ResetColor();
     }
 }
